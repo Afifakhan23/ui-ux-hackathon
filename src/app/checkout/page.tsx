e4e5d94a561203e692c722c5ca8cf42ae96c8caf
Afifakhan23 <afifaak23@gmail.com>
2024-12-30 00:02:03 +0500

    UI & UX Hackathon

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,143 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Navbar from '../components/navbar'
+
+const Page = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="bg-contact bg-center bg-cover bg-no-repeat min-h-[350px] sm:min-h-[316px] w-full flex flex-col justify-center items-center">
+                <Image src={'/assets/logo.png'} alt='logo' width={77} height={77} />
+                <h2 className="text-[32px] sm:text-[40px] md:text-[60px] font-bold text-center">
+                    Checkout
+                </h2>
+                <p className="my-4 text-[16px] sm:text-[18px] md:text-[20px] font-normal text-center">
+                <Link href='/' className='font-bold'>Home</Link> <span className='font-bold w-[14px] h-[8px]'>{'>'}</span> Checkout
+                </p>
+            </div>
+            <div className='h-auto md:h-[1829px] w-full flex justify-center items-center'>
+            <div className='w-[320px] md:w-[1242px] h-auto md:h-[1714px] flex flex-col md:flex-row justify-evenly items-center md:items-start'>
+                <div className='w-[300px] md:w-[608px] h-[950px] md:h-[1714px] flex flex-col justify-evenly items-center text-[16px] font-medium'>
+                    <h2 className='text-[36px] font-semibold'>Billing details</h2>
+                    <div className='w-[300px] md:w-[453px] flex justify-between'>
+                    <label htmlFor="fname">
+                        First Name<br/>
+                        <input type="text" name="name" id="fname" className='w-[140px] md:w-[211px] h-14 md:h-[75px] border-2 border-mytextcolor rounded-[10px]' />
+                    </label>
+                    <label htmlFor="lname">
+                        Last Name<br/>
+                        <input type="text" name="name" id="lname" className='w-[140px] md:w-[211px] h-14 md:h-[75px] border-2 border-mytextcolor rounded-[10px]' />
+                    </label>
+                    </div>
+                    <label htmlFor="companyName">
+                    Company Name (Optional)<br/>
+                        <input type="text" name="name" id="companyName" className='w-[300px] md:w-[453px] h-14 md:h-[75px] border-2 border-mytextcolor rounded-[10px]' />
+                    </label>
+                    <label htmlFor="country">
+                    Country / Region<br/>
+                        <select name="country" id="country" className='w-[300px] md:w-[453px] h-14 md:h-[75px] border-2 border-mytextcolor rounded-[10px]'>
+                            <option value="SriLanka">Sri Lanka</option>
+                        </select>
+                    </label>
+                    <label htmlFor="streetAddress">
+                    Street address<br/>
+                        <input type="text" name="name" id="streetAddress" className='w-[300px] md:w-[453px] h-14 md:h-[75px] border-2 border-mytextcolor rounded-[10px]' />
+                    </label>
+                    <label htmlFor="Town">
+                    Town / City<br/>
+                        <input type="text" name="name" id="Town" className='w-[300px] md:w-[453px] h-14 md:h-[75px] border-2 border-mytextcolor rounded-[10px]' />
+                    </label>
+                    <label htmlFor="Province">
+                    Province<br/>
+                        <select name="country" id="Province" className='w-[300px] md:w-[453px] h-14 md:h-[75px] border-2 border-mytextcolor rounded-[10px]'>
+                            <option value="WesternProvince">Western Province</option>
+                        </select>
+                    </label>
+                    <label htmlFor="ZIPcode">
+                    ZIP code<br/>
+                        <input type="text" pattern="[0-9]{5}" name="name" id="ZIPcode" className='w-[300px] md:w-[453px] h-14 md:h-[75px] border-2 border-mytextcolor rounded-[10px]' />
+                    </label>
+                    <label htmlFor="Phone">
+                    Phone<br/>
+                        <input type="tel" name="Phone" id="Phone" className='w-[300px] md:w-[453px] h-14 md:h-[75px] border-2 border-mytextcolor rounded-[10px]' />
+                    </label>
+                    <label htmlFor="email">
+                    Email Address<br/>
+                        <input type="email" name="email" id="email" className='w-[300px] md:w-[453px] h-14 md:h-[75px] border-2 border-mytextcolor rounded-[10px]' />
+                    </label>
+                    <input type="text" name="Phone" id="Phone" placeholder='Additional information' className='w-[300px] md:w-[453px] h-14 md:h-[75px] border-2 border-mytextcolor rounded-[10px]' />
+                </div>
+                <div className='w-[320px] md:w-[608px] h-auto md:h-[789px] flex justify-center items-center mt-5 md:mt-32'>
+                    <ul className='w-[320px] md:w-[533px] h-auto md:h-[616px] flex flex-col justify-evenly items-center md:items-start'>
+                        <li className='w-[300px] md:w-[533px] flex justify-between items-center'>
+                            <h2 className='text-[24px] font-medium'>Product</h2>
+                            <h2 className='text-[24px] font-medium'>Subtotal</h2>
+                        </li>
+                        <li className='w-[300px] md:w-[533px] flex justify-between items-center'>
+                            <p className='text-mytextcolor font-normal'>Asgaard sofa <span className='text-black font-medium text-[12px]'>X 1</span>
+                            </p>
+                            <p>Rs. 250,000.00</p>
+                        </li>
+                        <li className='w-[300px] md:w-[533px] flex justify-between items-center'>
+                            <p className='font-normal'>Subtotal</p>
+                            <p>Rs. 250,000.00</p>
+                        </li>
+                        <li className='w-[300px] md:w-[533px] flex justify-between items-center pb-10 border-b-2 border-mylightblack'>
+                            <p>Total</p>
+                            <p className='font-bold text-[24px] text-[#B88E2F]'>Rs. 250,000.00</p>
+                        </li>
+                        <li className='py-10'>
+                            <label htmlFor="">
+                            <input type="radio" name="name" id="bank" className='accent-black mr-5' />
+                            Direct Bank Transfer
+                            </label>
+                        </li>
+                        <li>
+                            <p className='w-[300px] md:w-[528px]'>Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order will not be shipped until the funds have cleared in our account.</p>
+                        </li>
+                        <li className='mt-5'>
+                        <label htmlFor="">
+                            <input type="radio" name="name" id="bank" className='accent-black mr-5' />
+                            Direct Bank Transfer
+                            </label><br/>
+                            <label htmlFor="">
+                            <input type="radio" name="name" id="bank" className='accent-black mr-5' />
+                            Cash on Delivery
+                            </label>
+                        </li>
+                        <li className='mt-5'>
+                            <p className='w-[300px] md:w-[528px]'>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our privacy policy.</p>
+                        </li>
+                        <li className='w-[320px] md:w-[533px] flex justify-center items-center mt-5'>
+                        <button className='text-[20px] font-normal w-[280px] md:w-[318px] h-[64px] border-2 border-black rounded-[15px] flex justify-center items-center hover:bg-black hover:text-white'>Place order</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+            <div className="flex justify-evenly items-center bg-mylightpink w-full h-[300px] text-center mt-5">
+                <div className='w-[90px] md:w-[376px] h-[108px] '>
+                    <h3 className="text-lg font-medium">Free Delivery</h3>
+                    <p className="text-sm text-gray-600">
+                        For all orders over $50, consectetur adipiscing elit.
+                    </p>
+                </div>
+                <div className='w-[90px] md:w-[376px] h-[108px] '>
+                    <h3 className="text-lg font-medium">90 Days Return</h3>
+                    <p className="text-sm text-gray-600">
+                        If goods have problems, consectetur adipiscing elit.
+                    </p>
+                </div>
+                <div className='w-[90px] md:w-[376px] h-[108px] '>
+                    <h3 className="text-lg font-medium">Secure Payment</h3>
+                    <p className="text-sm text-gray-600">
+                        100% secure payment, consectetur adipiscing elit.
+                    </p>
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default Page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,9 @@ import { LuSearch } from "react-icons/lu";
 import { GoHeart } from "react-icons/go";
 import { BsCart3 } from "react-icons/bs";
 import { HiOutlineMenu, HiX } from "react-icons/hi";
+import { MdCancel } from "react-icons/md";
+import { BsBagX } from "react-icons/bs";
+import Image from 'next/image'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -25,58 +28,173 @@ import OurInstagram from "./components/ourInstagram";
 
 export default function Home() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCart = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-    <div className={`h-[100px] w-full ${poppins.className} bg-mydarkorange flex justify-end items-center`}>
+      <div className={`h-[100px] w-full ${poppins.className} bg-mydarkorange flex justify-end items-center`}>
+        {/* Desktop Navigation */}
+        <ul className="hidden lg:flex w-[430px] h-[24px] font-medium text-[16px] space-x-8 mr-0 lg:mr-40 ">
+          <li><Link href={'/'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>Home</Link></li>
+          <li><Link href={'/shop'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>Shop</Link></li>
+          <li><Link href={'/about'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>About</Link></li>
+          <li><Link href={'/contact'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>Contact</Link></li>
+        </ul>
 
-{/* Desktop Navigation */}
-<ul className="hidden lg:flex w-[430px] h-[24px] font-medium text-[16px] space-x-8 mr-0 lg:mr-40 ">
-  <li><Link href={'/'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>Home</Link></li>
-  <li><Link href={'/shop'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>Shop</Link></li>
-  <li><Link href={'/about'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>About</Link></li>
-  <li><Link href={'/contact'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>Contact</Link></li>
-</ul>
+        {/* Desktop Icons */}
+        <ul className="hidden lg:flex h-[28px] space-x-6 mr-0 lg:mr-20">
+          <li><Link href='/account'><FaRegUser className='w-[28px] h-[28px] hover:text-[#3867d6]' /></Link></li>
+          <li><LuSearch className='w-[28px] h-[28px] hover:text-mytextcolor' /></li>
+          <li><GoHeart className='w-[28px] h-[28px] hover:text-[#ff3f34]' /></li>
+          <div className="relative">
+            {/* Button to open the cart */}
+            <BsCart3 className='w-[28px] h-[28px] hover:text-[#3867d6]' onClick={toggleCart} />
 
-{/* Desktop Icons */}
-<ul className="hidden lg:flex h-[28px] space-x-6 mr-0 lg:mr-20">
-  <li><Link href='/account'><FaRegUser className='w-[28px] h-[28px] hover:text-[#3867d6]' /></Link></li>
-  <li><LuSearch className='w-[28px] h-[28px] hover:text-mytextcolor' /></li>
-  <li><GoHeart className='w-[28px] h-[28px] hover:text-[#ff3f34]' /></li>
-  <li><Link href='/cart'><BsCart3 className='w-[28px] h-[28px] hover:text-[#3867d6]' /></Link></li>
-</ul>
+            {/* Overlay */}
+            {isOpen && (
+              <div
+                className="fixed inset-0 bg-black bg-opacity-70 transition-opacity z-40"
+                onClick={toggleCart}
+              />
+            )}
 
-{/* Mobile Menu Button */}
-<button
-  className="lg:hidden text-black text-2xl mr-4"
-  onClick={() => setIsMenuOpen(!isMenuOpen)}
->
-  {isMenuOpen ? <HiX /> : <HiOutlineMenu />}
-</button>
+            {/* Shopping Cart Sidebar */}
+            <div
+              className={`fixed top-0 right-0 h-full w-80 flex flex-col justify-between bg-white shadow-lg transform transition-transform duration-300 z-50 ${isOpen ? "translate-x-0" : "translate-x-full"
+                }`}
+            >
+              <div>
+                {/* Header */}
+                <div className="flex items-center justify-between p-4 border-b">
+                  <h2 className="text-lg font-bold">Shopping Cart</h2>
+                  <BsBagX onClick={toggleCart} className="text-gray-600 hover:text-red-500 text-2xl" />
+                </div>
 
-{/* Mobile Menu */}
-{isMenuOpen && (
-  <div className="absolute top-[100px] left-0 w-full bg-mydarkorange text-black flex flex-col items-center space-y-6 py-6 lg:hidden">
-    <ul className="flex flex-col items-center space-y-4 font-medium text-[16px]">
-      <li><Link href={'/'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#FF5733]'>Home</Link></li>
-      <li><Link href={'/shop'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>Shop</Link></li>
-      <li><Link href={'/about'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>About</Link></li>
-      <li><Link href={'/contact'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>Contact</Link></li>
-    </ul>
-    <ul className="flex space-x-6">
-      <li><Link href={'/account'}><FaRegUser className='w-[28px] h-[28px] hover:text-[#3867d6]' /></Link></li>
-      <li><LuSearch className='w-[28px] h-[28px] hover:text-mytextcolor' /></li>
-      <li><GoHeart className='w-[28px] h-[28px] hover:text-[#ff3f34]' /></li>
-      <li><Link href={'/cart'}><BsCart3 className='w-[28px] h-[28px] hover:text-[#3867d6]' /></Link></li>
-    </ul>
-  </div>
-)}
-</div>
-    <Hero/>
-    <SideTable/>
-    <Picks/>
-    <NewArrivals/>
-    <MyBlogs/>
-    <OurInstagram/>
+                {/* Cart Item */}
+                <div className="flex items-center space-x-4 p-4">
+                  <div className="w-16 h-16 bg-yellow-100 flex items-center justify-center rounded-md">
+                    <Image src={'/assets/asgaardsofa.png'} alt='pic' width={100} height={100} />
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-gray-800 font-semibold">Asgaard Sofa</p>
+                    <p className="text-sm">
+                      1 x <span className="text-yellow-600">Rs. 250,000.00</span>
+                    </p>
+                  </div>
+                  <MdCancel className="text-gray-500 hover:text-red-600 text-2xl" />
+                </div>
+              </div>
+
+              <div className='flex flex-col'>
+                {/* Subtotal */}
+                <div className="p-4 mt-4 border-b flex justify-between items-center">
+                  <p className="text-lg font-semibold">Subtotal</p>
+                  <p className="text-yellow-600 font-bold">Rs. 250,000.00</p>
+                </div>
+
+                {/* Buttons */}
+                <div className="flex justify-around p-4">
+                  <Link href='/cart' className="border border-black text-black px-6 py-2 rounded-full hover:bg-black hover:text-white">
+                    View Cart
+                  </Link>
+                  <Link href='/checkout' className="border border-black text-black px-6 py-2 rounded-full hover:bg-black hover:text-white">
+                    Checkout
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </ul>
+        {/* Mobile Menu Button */}
+        <button
+          className="lg:hidden text-black text-2xl mr-4"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          {isMenuOpen ? <HiX /> : <HiOutlineMenu />}
+        </button>
+
+        {/* Mobile Menu */}
+        {isMenuOpen && (
+          <div className="absolute top-[100px] left-0 w-full bg-mydarkorange text-black flex flex-col items-center space-y-6 py-6 lg:hidden">
+            <ul className="flex flex-col items-center space-y-4 font-medium text-[16px]">
+              <li><Link href={'/'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#FF5733]'>Home</Link></li>
+              <li><Link href={'/shop'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>Shop</Link></li>
+              <li><Link href={'/about'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>About</Link></li>
+              <li><Link href={'/contact'} className='hover:text-[#FF5733] p-3 border-transparent border-b-2 hover:border-[#fa8231]'>Contact</Link></li>
+            </ul>
+            <ul className="flex space-x-6">
+              <li><Link href={'/account'}><FaRegUser className='w-[28px] h-[28px] hover:text-[#3867d6]' /></Link></li>
+              <li><LuSearch className='w-[28px] h-[28px] hover:text-mytextcolor' /></li>
+              <li><GoHeart className='w-[28px] h-[28px] hover:text-[#ff3f34]' /></li>
+              <div className="relative">
+                {/* Button to open the cart */}
+                <BsCart3 className='w-[28px] h-[28px] hover:text-[#3867d6]' onClick={toggleCart} />
+
+                {/* Shopping Cart Sidebar */}
+                <div
+                  className={`fixed top-0 right-0 h-full w-80 flex flex-col justify-between bg-white shadow-lg transform transition-transform duration-300 ${isOpen ? "translate-x-0" : "translate-x-full"
+                    }`}
+                >
+                  <div>
+                    {/* Header */}
+                    <div className="flex items-center justify-between p-4 border-b">
+                      <h2 className="text-lg font-bold">Shopping Cart</h2>
+                      <BsBagX onClick={toggleCart} className="text-gray-600 hover:text-red-500 text-2xl" />
+                    </div>
+
+                    {/* Cart Item */}
+                    <div className="flex items-center space-x-4 p-4">
+                      {/* Item Image */}
+                      <div className="w-16 h-16 bg-yellow-100 flex items-center justify-center rounded-md">
+                        <Image src={'/assets/asgaardsofa.png'} alt='pic' width={100} height={100} />
+                      </div>
+
+                      {/* Item Details */}
+                      <div className="flex-1">
+                        <p className="text-gray-800 font-semibold">Asgaard Sofa</p>
+                        <p className="text-sm">
+                          1 x <span className="text-yellow-600">Rs. 250,000.00</span>
+                        </p>
+                      </div>
+
+                      {/* Remove Item Button */}
+                      <MdCancel className="text-gray-500 hover:text-red-600 text-2xl" />
+                    </div>
+                  </div>
+
+
+                  <div className='flex flex-col'>
+                    {/* Subtotal */}
+                    <div className="p-4 mt-4 border-b flex justify-between items-center">
+                      <p className="text-lg font-semibold">Subtotal</p>
+                      <p className="text-yellow-600 font-bold">Rs. 250,000.00</p>
+                    </div>
+
+                    {/* Buttons */}
+                    <div className="flex justify-around p-4">
+                      <Link href='/cart' className="border border-black text-black px-6 py-2 rounded-full hover:bg-black hover:text-white">
+                        View Cart
+                      </Link>
+                      <Link href='/checkout' className="border border-black text-black px-6 py-2 rounded-full hover:bg-black hover:text-white">
+                        Checkout
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </ul>
+          </div>
+        )}
+      </div>
+      <Hero />
+      <SideTable />
+      <Picks />
+      <NewArrivals />
+      <MyBlogs />
+      <OurInstagram />
     </>
   );
 }
