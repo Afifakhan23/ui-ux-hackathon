bf28a1a028a6ee3a2424918deb9d2ca34acada60
Afifakhan23 <afifaak23@gmail.com>
2025-02-07 02:17:30 +0500

    Hackathon Website

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,11 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
+// You can define custom matchers here if needed
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)", 
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
